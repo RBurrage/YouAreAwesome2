6fdf1a66ebf89795acf42c1de8f6d48d2ba638f2
Robin Burrage <robin.o.burrage@gmail.com>
2026-02-25 14:17:54 -0500

    Initial Commit

---
+++ YouAreAwesome2/YouAreAwesome2App.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesome2App.swift
+//  YouAreAwesome2
+//
+//  Created by Robin Burrage on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesome2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
